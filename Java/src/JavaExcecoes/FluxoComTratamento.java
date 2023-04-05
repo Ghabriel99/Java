7104835dcede28b76a1506d873d42084b57f1cdc
Ghabriel Marchesi Schiavo <86672683+Ghabriel99@users.noreply.github.com>
2023-04-05 16:57:52 -0300

    .
    .

--- Java/src/JavaExcecoes/FluxoComTratamento.java
+++ Java/src/JavaExcecoes/FluxoComTratamento.java
@@ -1,14 +1,15 @@
 package JavaExcecoes;
 
-public class Fluxo {
+public class FluxoComTratamento {
 
    public static void main(String[] args) {
       System.out.println("Ini do main");
       try {
          metodo1();
-      } catch(ArithmeticException | MinhaExcecao ex) {
+
+      } catch(ArithmeticException | NullPointerException ex) {
          String msg = ex.getMessage();
-         System.out.println("Arithimetic excepttion" + msg);
+         System.out.println("Exception " + msg);
          ex.printStackTrace();
       }
       
@@ -17,18 +18,17 @@ public class Fluxo {
 
    private static void metodo1() {
       System.out.println("Ini do metodo1");
-      try {
-         metodo2();
-      } catch (Exception e) {
-         System.out.println(e);
-      }
+     
+      metodo2();
       
       System.out.println("Fim do metodo1");
    }
 
    private static void metodo2() {
       System.out.println("Ini do metodo2");
-      throw new MinhaExcecao("deu b.o");
-      // System.out.println("Fim do metodo2");
+
+      throw new ArithmeticException("ERROR");
+      
+      //System.out.println("Fim do metodo2");
    }
 }
